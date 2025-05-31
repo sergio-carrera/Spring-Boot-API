52358b6d680d5fcd051ae9b73758782e3a4a6420
Sergio Carrera <sergiocarrerahidalgo@gmail.com>
2025-05-31 08:51:18 -0600

    Cargando inicio de proyecto

---
+++ backend/product-api-backend-springboot/src/main/java/com/example/product_api_backend_springboot/ProductApiBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.product_api_backend_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiBackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiBackendSpringbootApplication.class, args);
+	}
+
+}
---
+++ backend/product-api-backend-springboot/src/test/java/com/example/product_api_backend_springboot/ProductApiBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.product_api_backend_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiBackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
